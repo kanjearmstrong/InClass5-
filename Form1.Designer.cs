2d29ffd1526e7d3a4562cdc2a9ccadb70b5be68a
kanjearmstrong <47827157+kanjearmstrong@users.noreply.github.com>
2019-02-23 16:57:36 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Inclass5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtNumterms = new System.Windows.Forms.TextBox();
+            this.txtCalc = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter # of terms:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(56, 246);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(102, 42);
+            this.btnCalc.TabIndex = 1;
+            this.btnCalc.Text = "CALCULATE";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNumterms
+            // 
+            this.txtNumterms.Location = new System.Drawing.Point(213, 79);
+            this.txtNumterms.Name = "txtNumterms";
+            this.txtNumterms.Size = new System.Drawing.Size(100, 20);
+            this.txtNumterms.TabIndex = 2;
+            // 
+            // txtCalc
+            // 
+            this.txtCalc.Location = new System.Drawing.Point(213, 258);
+            this.txtCalc.Name = "txtCalc";
+            this.txtCalc.Size = new System.Drawing.Size(100, 20);
+            this.txtCalc.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 389);
+            this.Controls.Add(this.txtCalc);
+            this.Controls.Add(this.txtNumterms);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Approximate PI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtNumterms;
+        private System.Windows.Forms.TextBox txtCalc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inclass5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtCalc.Clear();
+
+            Refresh();
+
+            double pi_over_4 = 0;
+            int num_terms = int.Parse(txtNumterms.Text);
+            double sign = 1;
+            for(int term = 0; term < num_terms; term++)
+            {
+                pi_over_4 += sign / (term * 2 + 1);
+                sign *= -1;
+            }
+            double pi = 4 * pi_over_4;
+            txtCalc.Text = pi.ToString();
+                }
+            }
+        }
